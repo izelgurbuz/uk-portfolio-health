c3f15d3c9634428828a3649b7ab813e5b7ea8992
Izel Gurbuz <izel@izel.local>
2025-09-16 18:50:57 +0100

    etl Dag

--- airflow/dags/etl_uk_portfolio_health.py
+++ airflow/dags/etl_uk_portfolio_health.py
@@ -2,27 +2,44 @@ from datetime import datetime, timedelta
 
 from airflow.models.baseoperator import chain
 from airflow.operators.python import PythonOperator
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
 
 from airflow import DAG
+from pipeline.jobs.data_quality import dq_check
+from pipeline.jobs.incremental_load import main as run_incremental_main
 from pipeline.jobs.load_to_snowflake import apply_ddl as sf_apply_ddl
-from pipeline.jobs.load_to_snowflake import load_raw as sf_load_raw
-from pipeline.jobs.run_local_etl import main as run_local_etl_main
 
 default_args = {"owner": "izel", "retries": 2, "retry_delay": timedelta(minutes=5)}
 
 with DAG(
     dag_id="etl_uk_portfolio_health",
     start_date=datetime(2024, 1, 1),
-    schedule_interval="0 6 * * 1-5",
+    schedule_interval="0 6 * * 1-5",  # Run daily(weekdays) at 06:00
     catchup=False,
     default_args=default_args,
     max_active_runs=1,
-    tags=["portfolio", "etl", "elt", "uk"],
+    tags=["portfolio", "etl", "analytics"],
 ) as dag:
     t_apply_ddl = PythonOperator(task_id="apply_ddl", python_callable=sf_apply_ddl)
-    t_extract_clean_local = PythonOperator(
-        task_id="extract_clean_local", python_callable=run_local_etl_main
-    )
-    t_load_raw = PythonOperator(task_id="load_raw", python_callable=sf_load_raw)
 
-    chain(t_apply_ddl, t_extract_clean_local, t_load_raw)
+    t_incremental_load = PythonOperator(
+        task_id="incremental_load",
+        python_callable=run_incremental_main,
+    )
+
+    t_dq_check = PythonOperator(
+        task_id="dq_check",
+        python_callable=dq_check,
+    )
+    t_build_fact_prices = SnowflakeOperator(
+        task_id="build_fact_prices",
+        sql="sql/03_analytics_views.sql",
+        snowflake_conn_id="snowflake_default",
+    )
+
+    chain(
+        t_apply_ddl,
+        t_incremental_load,
+        t_dq_check,
+        t_build_fact_prices,
+    )
